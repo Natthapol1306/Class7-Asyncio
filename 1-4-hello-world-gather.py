46610c7ae10ea6c9ad47ebe8ac8aad16e0e41428
Natthapol1306 <71426141+Natthapol1306@users.noreply.github.com>
2023-10-24 16:05:47 +0700

    Update 1-4-hello-world-gather.py

--- 1-4-hello-world-gather.py
+++ 1-4-hello-world-gather.py
@@ -1,16 +1,19 @@
+#1-4-hello-world-gather.py
+
 import asyncio
 import time
 
 async def print_after(message, delay):
-    # Print a message after the specified delay (in seconds)
+    """Print a message after the specified delay (in second)"""
     await asyncio.sleep(delay)
-    print(f"{time.ctime()} - {message}")
+    print(f"{time.ctime()} - {message}")   
+
 
 async def main():
-    # Use asyncio.gather to run 2 coroutines concurrently:
+    # Use asyncio.gather to run two coroutines concurrently:
     await asyncio.gather(
         print_after("World!", 2),
         print_after("Hello", 1)
-    )
+    ) 
 
 asyncio.run(main())
